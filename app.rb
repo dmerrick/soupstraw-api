43dba87ab6b4f72cd2a79b60fe44e7c47b0c389f
Dana Merrick <dmerrick@brightcove.com>
2013-11-19 15:10:11 -0500

    Adding better errors for development.

--- app.rb
+++ app.rb
@@ -15,10 +15,16 @@ Dir['./helpers/*.rb'].each { |file| require file }
 
 class SoupstrawAPI < Sinatra::Base
 
-  get '/' do
-    'hello, world'
+  configure :development do
+    use BetterErrors::Middleware
+    # need to set in order to abbreviate filenames
+    BetterErrors.application_root = File.expand_path('..', __FILE__)
   end
 
   # start the server if ruby file executed directly
-  run! if app_file == $0
+  run! if app_file == $PROGRAM_NAME
+
+  get '/' do
+    'hello, world'
+  end
 end
