d06b1bf9c45a7336312447d5377f88a76921be2a
Dana Merrick <dmerrick@brightcove.com>
2013-11-26 23:17:10 -0800

    Adding simple bladehealth route.

--- routes/deafguy.rb
+++ routes/deafguy.rb
@@ -2,4 +2,9 @@ class SoupstrawAPI < Sinatra::Base
   get '/?' do
     'hello, deafguy'
   end
+
+  get '/bladehealth', auth: :authorized do
+    #FIXME: this needs JSON or something
+    `/usr/local/bin/bladehealth`
+  end
 end
