0276d2a7b162ff29cade8f99ef0c7da8440470ee
Dana Merrick <dmerrick@brightcove.com>
2013-11-28 01:23:19 -0800

    Adding more data to the lights route.

--- routes/lights.rb
+++ routes/lights.rb
@@ -3,8 +3,9 @@ class SoupstrawAPI < Sinatra::Base
   get '/lights', auth: :authorized do
     content_type :json
     lights = {}
+    #TODO: add a Light#to_json method to philips_hue gem
     settings.hue.lights.inject(lights) do |hash, light|
-      hash[light.light_id] = { name: light.name }
+      hash[light.light_id] = light.state.merge(name: light.name)
       hash
     end
     lights.to_json
