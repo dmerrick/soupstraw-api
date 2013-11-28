6bb2e8349ba1e1c541d698425610d1f2459c10ee
Dana Merrick <dmerrick@brightcove.com>
2013-11-28 01:10:32 -0800

    Adding a lights route.

--- routes/lights.rb
+++ routes/lights.rb
@@ -1,6 +1,17 @@
 class SoupstrawAPI < Sinatra::Base
 
+  get '/lights', auth: :authorized do
+    content_type :json
+    lights = {}
+    settings.hue.lights.inject(lights) do |hash, light|
+      hash[light.light_id] = { name: light.name }
+      hash
+    end
+    lights.to_json
+  end
+
   get '/lights/on', auth: :authorized do
+    content_type :json
     settings.hue.lights.each do |light|
       light.on!
     end
@@ -9,6 +20,7 @@ class SoupstrawAPI < Sinatra::Base
   end
 
   get '/lights/off', auth: :authorized do
+    content_type :json
     settings.hue.lights.each do |light|
       light.off!
     end
