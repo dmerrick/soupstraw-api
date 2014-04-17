dd3b003cad6e661b67f28b8f9868ccf588c294ee
Dana Merrick <dmerrick@brightcove.com>
2014-04-17 15:07:28 -0700

    Adding stop/prev/next controls.

--- app/routes/media_center.rb
+++ app/routes/media_center.rb
@@ -18,4 +18,25 @@ class SoupstrawAPI < Sinatra::Base
     `#{tell_iTunes_to('pause')}`.to_json
   end
 
+  # not authorized cause it's on the home network
+  get '/itunes/stop' do
+    pass unless is_media_center?
+    content_type :json
+    `#{tell_iTunes_to('stop')}`.to_json
+  end
+
+  # not authorized cause it's on the home network
+  get '/itunes/next' do
+    pass unless is_media_center?
+    content_type :json
+    `#{tell_iTunes_to('next track')}`.to_json
+  end
+
+  # not authorized cause it's on the home network
+  get '/itunes/previous' do
+    pass unless is_media_center?
+    content_type :json
+    `#{tell_iTunes_to('previous track')}`.to_json
+  end
+
 end
